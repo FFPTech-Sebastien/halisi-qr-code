8645ec3287d488c9ff48744ca0e27a37b48b5cd9
FFPTech-Sébastien <sebastien.vanvreckem@gmail.com>
2022-03-23 15:15:17 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import QRCode from "react-qr-code";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <QRCode
+        value={`exp://192.168.0.134:19000?room=rea541r68aze6r248r4ezr#çrea!rerazer4`}
+      />
     </div>
   );
 }
