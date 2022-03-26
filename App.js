881067a3be11c23241ce0709d00d06737cfc8f6f
FFPTech-Sebsatien <sebastien.vanvreckem@fitforpurpose.tech>
2022-03-26 09:40:26 +0100

    socket added

--- App.js
+++ App.js
@@ -1,39 +1,47 @@
 import React, { useState, useEffect } from "react";
 import { Text, View, StyleSheet, Button } from "react-native";
 import * as Linking from "expo-linking";
+import io from 'socket.io-client';
 
 export default function App() {
-  const [data, setData] = useState(null);
+	const [data, setData] = useState(null);
 
-  const handleOpenURL = (event) => {
-    let data = Linking.parse(event.url);
-    console.log(data);
-    setData(data);
-  };
+	const handleOpenURL = (event) => {
+		let data = Linking.parse(event.url);
+		console.log(data);
+		setData(data);
+	};
 
-  useEffect(() => {
-    const getInitialURL = async () => {
-      const initialURL = await Linking.getInitialURL();
-      if (initialURL) setData(Linking.parse(initialURL));
-    };
+	const joinRoom = (room) => {
+		const socket = io("192.168.1.61:5000");
+		socket.emit("join", room);
+	}
 
-    if (!data) {
-      getInitialURL();
-    }
-  }, []);
+	useEffect(() => {
 
-  return (
-    <View style={styles.container}>
-      <Text>{data ? JSON.stringify(data) : "App not opened from link"}</Text>
-    </View>
-  );
+		(async () => {
+			const initialURL = await Linking.getInitialURL();
+			if (initialURL) {
+				const room = Linking.parse(initialURL).queryParams.room
+				setData(room);
+				joinRoom(room);
+			}
+		})();
+
+	}, []);
+
+	return (
+		<View style={styles.container}>
+			<Text>{data ? JSON.stringify(data) : "App not opened from link"}</Text>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
