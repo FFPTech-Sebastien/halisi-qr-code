432525a45318d8e32d4e05ad82a65769382e7e10
FFPTech-Sebsatien <sebastien.vanvreckem@fitforpurpose.tech>
2022-03-26 09:40:55 +0100

    socket added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
 import "./App.css";
 import QRCode from "react-qr-code";
+import { useEffect, useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
+import io from "socket.io-client";
+
+// const IP = "192.168.1.61:19000";
+// const IP = "ma-q28.ffptech-expo.mobile-app.exp.direct:80"
 
 function App() {
-  return (
-    <div className="App">
-      <QRCode
-        value={`exp://192.168.0.134:19000?room=rea541r68aze6r248r4ezr#çrea!rerazer4`}
-      />
-    </div>
-  );
+	const [value, setValue] = useState("");
+	const [isMobileAppConnected, setMobileAppConnected] = useState(false);
+
+	const generateQRCode = () => {
+		const id = uuidv4();
+		setValue(`exp://exp.host/@ffptech-expo/expo-face-detector?room=${id}`);
+	}
+
+	useEffect(() => {
+		const socket = io("192.168.1.61:5000");
+		socket.on("connected", () => {
+			setMobileAppConnected(true);
+		});
+	}, []);
+
+	return (
+		<div className="App">
+			{value &&
+				<QRCode
+					value={value}
+				/>}
+
+			<button onClick={generateQRCode}>Generate new QrCode</button>
+			{
+				isMobileAppConnected && (
+					<div>
+						<b>Mobile app connected</b>
+					</div>
+				)
+			}
+		</div>
+	);
 }
 
 export default App;
