3ebed547609735e5b8b579a519e8ba27a5749008
FFPTech-Sébastien <sebastien.vanvreckem@gmail.com>
2022-03-28 18:21:58 +0200

    join room

--- src/App.js
+++ src/App.js
@@ -1,45 +1,55 @@
 import "./App.css";
 import QRCode from "react-qr-code";
 import { useEffect, useState } from "react";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 import io from "socket.io-client";
 
 // const IP = "192.168.1.61:19000";
 // const IP = "ma-q28.ffptech-expo.mobile-app.exp.direct:80"
+// const IP = "exp.host/@ffptech-expo/expo-face-detector";
+const IP = "192.168.0.134";
+
+const socket = io(`${IP}:5000`);
 
 function App() {
-	const [value, setValue] = useState("");
-	const [isMobileAppConnected, setMobileAppConnected] = useState(false);
+  const [value, setValue] = useState("");
+  const [globalId, setGlobalId] = useState(null);
+  const [isMobileAppConnected, setMobileAppConnected] = useState(false);
 
-	const generateQRCode = () => {
-		const id = uuidv4();
-		setValue(`exp://exp.host/@ffptech-expo/expo-face-detector?room=${id}`);
-	}
+  const generateQRCode = () => {
+    if (globalId) {
+      socket.emit("leave", globalId);
+    }
+    setMobileAppConnected(false);
+    const id = uuidv4();
+    setGlobalId(id);
+    setValue(`exp://${IP}:19000?room=${id}`);
+    socket.emit("join", { room: id, type: "web" });
+  };
 
-	useEffect(() => {
-		const socket = io("192.168.1.61:5000");
-		socket.on("connected", () => {
-			setMobileAppConnected(true);
-		});
-	}, []);
+  useEffect(() => {
+    socket.on("mobile-connected", () => {
+      console.log("mobile connected");
+      setMobileAppConnected(true);
+    });
 
-	return (
-		<div className="App">
-			{value &&
-				<QRCode
-					value={value}
-				/>}
+    return () => {
+      socket.emit("leave", globalId);
+    };
+  }, []);
 
-			<button onClick={generateQRCode}>Generate new QrCode</button>
-			{
-				isMobileAppConnected && (
-					<div>
-						<b>Mobile app connected</b>
-					</div>
-				)
-			}
-		</div>
-	);
+  return (
+    <div className="App">
+      {value && <QRCode value={value} />}
+
+      <button onClick={generateQRCode}>Generate new QrCode</button>
+      {isMobileAppConnected && (
+        <div>
+          <b>Mobile app connected</b>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
